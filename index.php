d93bb9040b668bacc44a071b90c39ca5376ca861
Ashenn1 <sohasanad@gmail.com>
2019-11-19 21:49:54 +0200

    Styling Playbar Buttons

--- index.php
+++ index.php
@@ -24,6 +24,35 @@
 			</div>
 
 			<div id="nowPlayingCenter">
+				<div class="content playerControls">
+
+					<div class="buttons"> 
+						<button class="controlButton shuffle" title="Shuffle Button">
+							<img src="Assets/Images/icons/shuffle.png" alt="Shuffle">
+						</button>
+
+						<button class="controlButton previous" title="Previous Button">
+							<img src="Assets/Images/icons/previous.png" alt="Previous">
+						</button>
+
+						<button class="controlButton play" title="Play Button">
+							<img src="Assets/Images/icons/play.png" alt="Play">
+						</button>
+ 
+						<button class="controlButton pause" title="Pause Button" style="display: none;">
+							<img src="Assets/Images/icons/pause.png" alt="Pause">
+						</button>
+
+						<button class="controlButton next" title="Next Button">
+							<img src="Assets/Images/icons/next.png" alt="Next">
+						</button>
+
+						<button class="controlButton repeat" title="Repeat Button">
+							<img src="Assets/Images/icons/repeat.png" alt="Repeat">
+						</button>
+
+					</div>
+				</div>
 				
 			</div>
 
