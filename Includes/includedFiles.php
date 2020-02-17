2646cd2350ff8c6c3d2625e035021b14e77b4548
Ashenn1 <sohasanad@gmail.com>
2020-02-17 21:57:54 +0200

    "YourMusic" page done

--- Includes/includedFiles.php
+++ Includes/includedFiles.php
@@ -2,11 +2,21 @@
 
 	if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
 		include("Includes/config.php");
+		include("Includes/classes/User.php");
 		include("Includes/classes/Artist.php");
 		include("Includes/classes/Album.php");
 		include("Includes/classes/Song.php");
+		include("Includes/classes/Playlist.php");
+
+		if(isset($_GET['userLoggedIn'])){
+			$userLoggedIn = new User($con ,$_GET['userLoggedIn'] );
+		}
+		else{
+			echo "Username variable was not passed into the page. Check the openPage javascript function";
+			exit();
+		}
 	}
-	else{
+	else{ 
 		include("includes/header.php");
 		include("includes/footer.php");
 
--- yourMusic.php
+++ yourMusic.php
@@ -13,6 +13,37 @@
 			<button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
 		</div>
 
+		<?php
+
+			$username = $userLoggedIn->getUsername();
+
+			$playlistsQuery = mysqli_query($con, "SELECT * FROM playlist WHERE owner='$username'");
+
+			if(mysqli_num_rows($playlistsQuery) == 0)
+				echo "<span class='noResults'>You don't have any playlists yet</span>";
+
+			while($row = mysqli_fetch_array($playlistsQuery) ) {
+
+				$playlist  = new Playlist($con , $row);
+
+				echo "<div class='gridViewItem' role = 'link' tabindex = '0' 
+				onclick='openPage(\"playlist.php?id=".$playlist->getId()."\")'>
+
+						<div class='playlistImage'>
+							<img src='Assets/Images/icons/playlist.png'>
+						</div>
+
+						<div class='gridViewInfo'>"
+								. $playlist->getName() .
+						"</div>
+
+					</div>";
+
+		}
+	?>
+
 	</div>
+
+
 	
 </div>
